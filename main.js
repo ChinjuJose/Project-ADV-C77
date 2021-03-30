8b2675c4f1ff0abe03f932cbfa4828928459d997
ChinjuJose <73712484+ChinjuJose@users.noreply.github.com>
2021-03-30 12:18:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("My first Console project");
+var firstName = "Chinju";
+console.log(firstName);
+var lastName = "Jose";
+console.log(lastName);
+var name = firstName.concat(lastName);
+
+var num1 = 53;
+var num2 = 34;
+var sum = num1+num2;
+var difference = num1-num2;
+var product = num1*num2;
+var quotient = num1/num2;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
